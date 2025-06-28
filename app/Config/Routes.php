277cd51bb7f3185c796b5d003b23eca9e7c96b4f
iMuhammadAbdullahAwan <immuhammadabdullahawan@gmail.com>
2025-06-28 17:45:17 +0500

    Update routes to use Auth controller for login and registration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,15 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Auth;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::login');
+$routes->get('/register', 'Auth::register');
+$routes->post('/register', 'Auth::register', ['method' => 'post']);
+// $routes->get('/post', 'Auth::register', ['method' => 'post']);
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::login', ['method' => 'post']);
+$routes->get('/logout', 'Auth::logout');
